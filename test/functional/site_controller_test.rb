b5fe55e2015425a9063af8f6c206df3488292303
lukebayes <lbayes@patternpark.com>
2009-09-05 10:45:27 -0400

    Added tests for site index

--- test/functional/site_controller_test.rb
+++ test/functional/site_controller_test.rb
@@ -1,8 +1,25 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
+
+# Re-raise errors caught by the controller.
+class UsersController; def rescue_action(e) raise e end; end
 
 class SiteControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  context "GET to :index" do
+
+    context "with no user" do
+      setup { get :index }
+      should_require_login
+    end
+
+    context "with valid user" do
+      setup do
+        login_as(:quentin)
+        get :index
+      end
+
+      should_respond_with :success
+    end
   end
+
 end
